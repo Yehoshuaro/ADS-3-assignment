2fc3903c68db35691c15cb0693967bbdd8bc445e
Yehoshuaro <231132@astanait.edu.kz>
2024-05-10 22:12:46 +0500

    Merge Sort with Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+import java.util.Scanner;
 public class Main {
-
     public static void mergeSort(int[] array) {
         if (array.length > 1) {
             int[] left = new int[array.length / 2];
             int[] right = new int[array.length - left.length];
-
             System.arraycopy(array, 0, left, 0, left.length);
             System.arraycopy(array, left.length, right, 0, right.length);
 
@@ -14,14 +13,13 @@ public class Main {
         }
     }
     private static void merge(int[] array, int[] left, int[] right) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int i = 0; int j = 0; int k = 0;
 
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
                 array[k++] = left[i++];
-            } else {
+            }
+            else {
                 array[k++] = right[j++];
             }
         }
@@ -32,15 +30,20 @@ public class Main {
             array[k++] = right[j++];
         }
     }
-
     public static void main(String[] args) {
-        int[] array = {5, 2, 4, 1, 3};
-        mergeSort(array);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the Array: ");
+        int size = scanner.nextInt();
 
+        int[] array = new int[size];
+        System.out.println("Enter elements: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        mergeSort(array);
         System.out.println("Sorted Array:");
         for (int i : array) {
             System.out.print(i + " ");
         }
     }
 }
-
