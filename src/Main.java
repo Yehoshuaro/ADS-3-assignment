44822880b383db619d9aefb3c029fd6c2ec92e67
Yehoshuaro <231132@astanait.edu.kz>
2024-05-10 22:35:38 +0500

    Quick Sort with Scanner New (to deeply understand)

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,49 @@
 import java.util.Scanner;
-
 public class Main {
-
-    public static void SelectionSort(int[] numbers) {
-        for (int sortedPartSize = 0; sortedPartSize < numbers.length - 1; sortedPartSize++) {
-            int smallestIndex = sortedPartSize;
-            for (int j = sortedPartSize + 1; j < numbers.length; j++) {
-                if (numbers[j] < numbers[smallestIndex]) {
-                    smallestIndex = j;
-                }
-            }
-            if (smallestIndex != sortedPartSize) {
-                int temp = numbers[sortedPartSize];
-                numbers[sortedPartSize] = numbers[smallestIndex];
-                numbers[smallestIndex] = temp;
-            }
+    public static void quickSort(int[] array) {
+        sort(array, 0, array.length - 1);
+    }
+    private static void sort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivot = partition(array, low, high);
+            sort(array, low, pivot - 1);
+            sort(array, pivot + 1, high);
         }
     }
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Array size: ");
         int size = scanner.nextInt();
 
         int[] numbers = new int[size];
-        System.out.println("Enter elements of array: ");
+        System.out.println("Enter elements of array:");
         for (int i = 0; i < size; i++) {
             numbers[i] = scanner.nextInt();
         }
 
-        SelectionSort(numbers);
+        quickSort(numbers);
         System.out.println("Sorted Array: ");
         for (int number : numbers) {
             System.out.print(number + " ");
