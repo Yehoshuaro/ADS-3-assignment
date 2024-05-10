16e9ebdeb4b0bbfdf1cbda374540f7b946f73cca
Yehoshuaro <231132@astanait.edu.kz>
2024-05-10 22:22:17 +0500

    Insertion Sort with Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,20 @@
 import java.util.Scanner;
 public class Main {
-    public static void heapSort(int[] array) {
-        int n = array.length;
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
 
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(array, n, i);
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            int temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-
-            heapify(array, i, 0);
-        }
-    }
-    private static void heapify(int[] array, int n, int i) {
-        int the_largest = i;
-        int leftChild = 2 * i + 1;
-        int rightChild = 2 * i + 2;
-
-        if (leftChild < n && array[leftChild] > array[the_largest]) {
-            the_largest = leftChild;
-        }
-        if (rightChild < n && array[rightChild] > array[the_largest]) {
-            the_largest = rightChild;
-        }
-        if (the_largest != i) {
-            int swap = array[i];
-            array[i] = array[the_largest];
-            array[the_largest] = swap;
-
-            heapify(array, n, the_largest);
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
         }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the size of the array: ");
         int size = scanner.nextInt();
 
@@ -44,11 +24,11 @@ public class Main {
             array[i] = scanner.nextInt();
         }
 
-        heapSort(array);
+        insertionSort(array);
         System.out.println("Sorted array: ");
-        for (int i : array) {
-            System.out.print(i + " ");
+        for (int element : array) {
+            System.out.print(element + " ");
         }
+        System.out.println();
     }
 }
-
