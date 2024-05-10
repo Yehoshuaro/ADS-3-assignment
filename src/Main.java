607ab64072efc6ff483fb4dda4236d837a408e00
Yehoshuaro <231132@astanait.edu.kz>
2024-05-10 09:58:57 +0500

    Merge Sort

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,41 @@
 public class Main {
 
-    public static void quickSort(int[] array, int low, int high) {
-        if (low < high) {
-            int pivot = partition(array, low, high);
-            quickSort(array, low, pivot - 1);
-            quickSort(array, pivot + 1, high);
+    public static void mergeSort(int[] array) {
+        if (array.length > 1) {
+            int[] left = new int[array.length / 2];
+            int[] right = new int[array.length - left.length];
+
+            System.arraycopy(array, 0, left, 0, left.length);
+            System.arraycopy(array, left.length, right, 0, right.length);
+
+            mergeSort(left);
+            mergeSort(right);
+            merge(array, left, right);
         }
     }
-    private static int partition(int[] array, int low, int high) {
-        int pivot = array[high];
-        int i = (low - 1);
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-                i++;
-                swap(array, i, j);
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
             }
         }
-        swap(array, i + 1, high);
-        return i + 1;
-    }
-
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
     }
 
     public static void main(String[] args) {
         int[] array = {5, 2, 4, 1, 3};
-        quickSort(array, 0, array.length - 1);
+        mergeSort(array);
 
         System.out.println("Sorted Array:");
         for (int i : array) {
@@ -37,3 +43,4 @@ public class Main {
         }
     }
 }
+
